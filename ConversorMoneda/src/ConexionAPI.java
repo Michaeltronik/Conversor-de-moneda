21c7d2849c0a54be4a4b86276f4ec4531d8515ca
Michaeltronik <michaelrts@hotmail.com>
2024-10-10 17:59:06 -0500

    creacion de menú principal básico y obtencion del json

--- ConversorMoneda/src/ConexionAPI.java
+++ ConversorMoneda/src/ConexionAPI.java
@@ -1,3 +1,37 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConexionAPI {
+    private String monedaBase;
+    private String monedaDestino;
+    private double cantidad;
+
+    public ConexionAPI(String monedaBase, String monedaDestino, double cantidad) {
+        this.monedaDestino = monedaDestino;
+        this.monedaBase = monedaBase;
+        this.cantidad = cantidad;
+    }
+
+    public String consultaAPI() throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccionAPI()))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        System.out.println(json);
+        return json;
+    }
+    public String direccionAPI (){
+        String direccion = "https://v6.exchangerate-api.com/v6/4c0bfb95c42208f8c572816b/pair/" +
+                monedaBase + "/" + monedaDestino + "/" + cantidad;
+        System.out.println(direccion);
+        return direccion;
+    }
 
 }
--- ConversorMoneda/src/Main.java
+++ ConversorMoneda/src/Main.java
@@ -1,5 +1,64 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String monedaBase = "" ;
+        String monedaDestino = "";
+        double cantidad = 0;
+        Scanner teclado = new Scanner(System.in);
+        int opcion = -1;
+        System.out.println("Bienvenid@ al conversor de moneda");
+        while(opcion != 7) {
+
+            System.out.println("Elija una opcion");
+            System.out.println("1. Dolar =>> peso Argentino \n" +
+                    "2. peso Argentino =>> Dolar\n" +
+                    "3. Dolar =>> Real Brasileño\n" +
+                    "4. Real Brasileño =>> Dolar\n" +
+                    "5. Dolar =>> peso Colombiano\n" +
+                    "6. peso Colombiano =>> Dolar\n");
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+            System.out.println("Ingrese el valor (monto) que desea convertir");
+            cantidad = teclado.nextDouble();
+            teclado.nextLine();
+
+            switch (opcion){
+                case 1:
+                    monedaBase = "USD";
+                    monedaDestino = "ARS";
+                    break;
+                case 2:
+                    monedaBase = "ARS";
+                    monedaDestino = "USD";
+                    break;
+                case 3:
+                    monedaBase = "USD";
+                    monedaDestino = "BRL";
+                    break;
+                case 4:
+                    monedaBase = "BRL";
+                    monedaDestino = "USD";
+                    break;
+                case 5:
+                    monedaBase = "USD";
+                    monedaDestino = "COP";
+                    break;
+                case 6:
+                    monedaBase = "COP";
+                    monedaDestino = "USD";
+                    break;
+                case 7:
+                    System.out.println("Gracias, vuelve pronto");
+                    break;
+                default:
+                    System.out.println("Opción no valida");
+                    break;
+            }
+
+            ConexionAPI consulta = new ConexionAPI(monedaBase, monedaDestino, cantidad);
+            consulta.consultaAPI();
+        }
     }
 }
